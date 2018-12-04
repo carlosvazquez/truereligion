255cc26df951790e944f826f0236f103aa3e255a
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2018-12-03 19:42:33 -0600

    First day, working in topbanner

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,31 +1,41 @@
 $(function() {
-    var win = $(window);
-    TrueReligion = {
-      _construct: function(){
-        currency = $('script#app').data("currency");
-      },
-      formatMoney: function(price){
-        var formatPrice = price /= 100;
-        formatPrice = formatPrice.toFixed(2);
-        formatPrice = formatPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        return '$' + formatPrice + ' ' + currency;
-      },
-      init: function() {
-        this._construct();
-        console.log('aaaa');
-      }
+  var TrueReligion = (function(window, $){
+    var _construct = function(){
+      currency = $('script#app').data("currency");
     }
+    var init = function() {
+      _construct();
+    }
+    var formatMoney = function(price){
+      var formatPrice = price /= 100;
+      formatPrice = formatPrice.toFixed(2);
+      formatPrice = formatPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      return '$' + formatPrice + ' ' + currency;
+    }
+    var alertMe = function() {
+      alert();
+    }
+    // Public methods
+    return {
+      init: init,
+      win: $(window),
+      formatMoney: formatMoney,
+      alertMe: alertMe
+    }
+  })(window, jQuery);
 
-    // Windows events chained
-    win.on('scroll', function() {
+  // Init App
+  TrueReligion.init();
 
-    })
-    .resize(function() {
-    })
-    .on("load", function () {
-    });
-    // Launch app
-    TrueReligion.init();
-    console.log(TrueReligion.formatMoney(210300));
-  }
-);
+  // Windows events chained
+  TrueReligion.win.on('scroll', function() {
+    console.log('on scroll');
+  })
+  .resize(function() {
+    console.log('on resize');
+  })
+  .on("load", function () {
+    console.log('on load');
+  });
+  console.log(TrueReligion.formatMoney(210300), TrueReligion.win, TrueReligion);
+});
